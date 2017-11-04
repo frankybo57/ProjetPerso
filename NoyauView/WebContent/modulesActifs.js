84e1ba5a3c141be79da09be2cca91c11b2927762
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-04 18:36:44 +0100

    Modules actifs
    Mise en place du fichier des modules actifs et import dans noyau.html

---
+++ NoyauView/WebContent/modulesActifs.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+var modulesActifs = [
+	{
+		"id" : 1,
+		"nom" : "Noyau",
+		"etat" : "Actif"
+	},
+	{
+		"id" : 2,
+		"nom" : "ModuleStock",
+		"etat" : "Actif"
+	},
+	{
+		"id" : 3,
+		"nom" : "ModuleRecette",
+		"etat" : "Actif"
+	},
+	{
+		"id" : 4,
+		"nom" : "ModuleCourse",
+		"etat" : "Inactif"
+	},
+	{
+		"id" : 5,
+		"nom" : "ModuleMenu",
+		"etat" : "Inactif"
+	},
+	{
+		"id" : 6,
+		"nom" : "ModuleCalculatrice",
+		"etat" : "Actif"
+	}
+];
