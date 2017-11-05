7c0d7e61ec33f3dd615b359dae89b5af7510721d
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-05 15:28:05 +0100

    Correctif utilisateur
    Suppression du difer

--- NoyauView/WebContent/User.js
+++ NoyauView/WebContent/User.js
@@ -1,10 +1,24 @@
 /**
  * 
  */
-utilisateurs = [
+var utilisateurs = [
 	{
 		"id" : 1,
 		"login" : "Verwalter",
 		"password" : "Passwort"
 	}
 	];
+
+var isUser = function(login){
+	for(var utilisateur of utilisateurs){
+		if(utilisateur.login === login)return true;
+	}
+	return false;
+};
+
+var getUserByLogin = function(login){
+	for(var utilisateur of utilisateurs){
+		if(utilisateur.login === login)return utilisateur;
+	}
+	return null;
+};
